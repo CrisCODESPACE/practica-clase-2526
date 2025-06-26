8ae8a7ac6c8de04e335d437390a26600376d21bb
RoarCriss <cristina94m@gmail.com>
2025-06-26 12:17:10 +0200

    fixed html views + added register ui

--- src/main.js
+++ src/main.js
@@ -4,23 +4,40 @@ const main = document.getElementById("main-container");
 
 // funcion para vistas dinámicas
 async function loadView(viewName) {
-  const response = await fetch(`views/${viewName}.html`);
+  const response = await fetch(`/views/${viewName}.html`);
   const html = await response.text();
 
   main.innerHTML = html;
 
-  // if (viewName === "register") aquí irá la funcion que muestre register
+  if (viewName === "register") uiRegister();
   // if (viewName === "login")  aquí irá la funcion que muestre login
   // if (viewName === "profile")  aquí irá la funcion que muestre profile
 }
 
 // llamada inicial por defecto a login
 
-loadView("login");
+loadView("register");
 
-createUser({
-  userName: "Paco",
-  userEmail: "pacopi@preuba.com",
-  password: "1254489",
-  userCountry: "España",  
-});
+// UI de register
+
+function uiRegister() {
+  const form = document.getElementById("register-form");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const regName = document.getElementById("reg-name").value;
+    const regEmail = document.getElementById("reg-email").value;
+    const regPassword = document.getElementById("reg-password").value;
+    const regCountry = document.getElementById("reg-country").value;
+
+    const userData = {
+      regName,
+      regEmail,
+      regPassword,
+      regCountry,
+    };
+
+    await createUser(userData);
+  });
+}
